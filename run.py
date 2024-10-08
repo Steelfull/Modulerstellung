54b340646900b34002c355da008e38fbc9a6b60d
Steelfull <tim-luka98@web.de>
2024-10-08 19:47:32 +0200

    Initial commit

--- run.py
+++ run.py
@@ -1,11 +1,4 @@
 from app import create_app
-import os
-from flask import Flask
-
-app = Flask(__name__,
-            static_folder=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'static'),
-            template_folder=os.path.join(os.path.abspath(os.path.dirname(__file__)), 'templates'))
-
 
 app = create_app()
 
